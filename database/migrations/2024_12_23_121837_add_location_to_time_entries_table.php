d8fdfb5158fddcf49114c6a39c18221c8c80153f
ashish13445 <jashish430.aj@gmail.com>
2024-12-23 12:20:03 +0530

    added location

---
+++ database/migrations/2024_12_23_121837_add_location_to_time_entries_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('time_entries', function (Blueprint $table) {
+            $table->text('location')->nullable(); // Replace 'column_name' with the column after which you want to place 'location'.
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('time_entries', function (Blueprint $table) {
+            $table->dropColumn('location');
+        });
+    }
+};
