0fda85a725ec3f554bb5e94362b2f833cdea2c48
ashish13445 <jashish430.aj@gmail.com>
2025-02-28 17:23:42 +0530

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -198,6 +198,7 @@ class UserController extends Controller
     {
         // Use the CheckRole middleware to ensure the user has the 'admin' role
         $users = Project::with('areas.users.timeEntries')->get();
+        dump($users);
         return response()->json($users);
        
      
