855da64bd4679366c5dc721e04be908997c01667
ashish13445 <jashish430.aj@gmail.com>
2025-03-04 15:34:41 +0530

    test

--- resources/js/Pages/Admin/Users.vue
+++ resources/js/Pages/Admin/Users.vue
@@ -1176,6 +1176,7 @@ const exportCSV = () => {
     project.areas.forEach((area) => {
       area.users.forEach((user) => {
         // Filter entries from the previous month
+        console.log(user.timeEntries);
         const previousMonthTimeEntries = user.timeEntries.filter((entry) => {
           const entryDate = new Date(entry.date);
           return (
