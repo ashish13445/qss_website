8c6db765cd0e73f7a2cbb07dc7318ab26ea9cc71
ashish13445 <jashish430.aj@gmail.com>
2025-04-17 12:04:14 +0530

    test

--- app/Models/TimeEntry.php
+++ app/Models/TimeEntry.php
@@ -9,4 +9,10 @@ class TimeEntry extends Model
 {
     use HasFactory;
     protected $fillable = ['user_id', 'clock_in', 'clock_out','date','shift_no','working_hours','remarks','location'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
